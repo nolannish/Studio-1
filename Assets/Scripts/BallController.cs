0a974b51a68615e57e117efbb9d03c105764c5b4
nolannish <nolannish@gmail.com>
2025-01-25 13:07:08 -0800

    Added InputManager and event listener (i think thats what its called)

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -3,38 +3,16 @@ using UnityEngine;
 public class BallController : MonoBehaviour
 {
 
-    public Rigidbody sphereRigidBody;
-    public float ballSpeed = 2f;
+    [SerializeField] private Rigidbody sphereRigidBody;
+    [SerializeField] private float ballSpeed = 2f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        Vector2 inputVector = Vector2.zero;
-
-        if(Input.GetKey(KeyCode.W)){
-            inputVector += Vector2.up;
-        }
-
-        if(Input.GetKey(KeyCode.S)){
-            inputVector += Vector2.down;
-        }
-
-        if(Input.GetKey(KeyCode.D)){
-            inputVector += Vector2.right;
-        }
-
-        if(Input.GetKey(KeyCode.A)){
-            inputVector += Vector2.left;
-        }
-
-        Vector3 inputXZPlane = new Vector3(inputVector.x, 0, inputVector.y);
-        Debug.Log("2d vector: " + inputVector);
-        Debug.Log("3D Vector: " + inputXZPlane);
+    public void MoveBall(Vector2 input){
+        Vector3 inputXZPlane = new(input.x, 0, input.y);
         sphereRigidBody.AddForce(inputXZPlane * ballSpeed);
     }
 }
